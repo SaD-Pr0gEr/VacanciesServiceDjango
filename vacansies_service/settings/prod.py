1ed14ff82c7d5d377c3dc05d86a452b08632ab3f
Ozodchik <dilovardehkanov730@gmail.com>
2022-01-31 17:43:42 +0300

    changed header, archived parser logic, changed prod confs

--- vacansies_service/settings/prod.py
+++ vacansies_service/settings/prod.py
@@ -16,10 +16,10 @@ ALLOWED_HOSTS = []
 DATABASES = {
     'default': {
         'ENGINE': "django.db.backends.postgresql",
-        'NAME': os.getenv("SQL_DATABASE"),
-        'USER': os.getenv("SQL_USER"),
-        'PASSWORD': os.getenv("SQL_PASSWORD"),
-        'HOST': os.getenv("SQL_HOST"),
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
         'PORT': "5432",
     }
 }
